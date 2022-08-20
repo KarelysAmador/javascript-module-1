6eac6d0c3e1648ca4f2d38af50c636f5ff65a57c
Karelys Amador <karelysamador95@gmail.com>
2022-08-20 13:11:58 +0200

    ejercicio J-1

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+const nombre = "Daniel";
+const nombreLength = nombre.length;
+const message= "My name is " + nombre + " and my name is " + nombreLength + " characters long";
 
 console.log(message);
+
--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-const name = " Daniel  ";
+const nombre = " Daniel   ";
+const nombreSinEspacio = nombre.trim()
+const nombreLength = nombreSinEspacio.length;
+const message = "My name is " + nombreSinEspacio + " and my name is " + nombreLength + " characters long";
 
 console.log(message);
