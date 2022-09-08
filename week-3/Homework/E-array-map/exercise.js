1687abdde3af77e580f1846758182d1e3f4fb2fb
Karelys Amador <karelysamador95@gmail.com>
2022-09-08 23:01:57 +0200

    ejercicio hecho

--- week-3/Homework/E-array-map/exercise.js
+++ week-3/Homework/E-array-map/exercise.js
@@ -1,5 +1,12 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+const numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+function multiples(number){
+    return number *100;
+}
+
+const numberMap = numbers.map(multiples);
+
+console.log(numberMap);
