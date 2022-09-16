caa4e43e078b383ff1248075d79c20af88c8e747
Karelys Amador <karelysamador95@gmail.com>
2022-09-16 21:41:57 +0200

    Corrección K

--- week-2/Homework/K-while-loop/exercise.js
+++ week-2/Homework/K-while-loop/exercise.js
@@ -2,21 +2,17 @@
 
 - Takes one number n as a parameter
 - Adds all numbers from 0 to n. For example, if the input is 3, the output should be 0 + 1 + 2 + 3
-- You should use a while loop
-
-*/
+- You should use a while loop*/
 
 let n = 10;
 
-function sumTillNum(num){
-	let i = 3
+function sumTillNum(num){ 
+	let i = 0 
 	let sum = 0
 	while (i <= num) {
-		i += 1
-		sum += i
+	 sum += i
+	 i += 1 
 	}
-	return sum;
-	//your code here
+	return sum
 }
-
 console.log("Sum from 0 to " + n + " is: " + sumTillNum(n));
